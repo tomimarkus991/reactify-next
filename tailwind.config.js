0f03e198fed846f87b762bc6eeb14b48034383af
tomimarkus991 <tomimarkusalber@gmail.com>
2023-03-04 14:54:26 +0200

    File changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,23 @@
 
 const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
+const flattenColorPalette = require("tailwindcss/lib/util/flattenColorPalette").default;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+const addVariablesForColors = ({ addBase, theme }) => {
+  const allColors = flattenColorPalette(theme("colors"));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  // darkMode: ["class", '[data-theme="dark"]'],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     container: {
@@ -31,11 +44,9 @@ module.exports = {
         xs2: "0.6rem",
       },
       colors: {
-        "surface-bg": "#F7F8FA",
+        textGray: "#292929",
         "text-primary": "#393939",
-        "tab-bg": "#F6F6F8",
-        primary: "#E50815",
-        secondary: "#E58D08",
+        textWhite: "#f3f2f0",
         casualRed: "#FF695A",
         casualOrange: "#FFA033",
         casualGreen: "#42D68C",
@@ -44,6 +55,25 @@ module.exports = {
         casualSlateBlue: "#97B8FC",
         casualViolet: "#C780E8",
         casualPink: "#F07DD7",
+        casualRedDarker: "#B92F27",
+        casualOrangeDarker: "#C76A2A",
+        casualGreenDarker: "#30A46A",
+        casualAquaDarker: "#05989D",
+        casualSkyBlueDarker: "#2A85D4",
+        casualSlateBlueDarker: "#728ABC",
+        casualVioletDarker: "#9756B5",
+        casualPinkDarker: "#CC41AE",
+        casualRedOutline: "#fcaaa1",
+        casualOrangeOutline: "#FFC684",
+        casualGreenOutline: "#79E2AE",
+        casualAquaOutline: "#26E2E9",
+        casualSkyBlueOutline: "#6DB7F8",
+        casualSlateBlueOutline: "#AAC6FF",
+        casualVioletOutline: "#E2A5FF",
+        casualPinkOutline: "#FFA8EC",
+        darkOutline: "#78808c",
+        lightOutline: "#eef2f6",
+        blueOutline: "#6585DF",
       },
       aspectRatio: {
         "4/3": "4 / 3",
@@ -95,5 +125,6 @@ module.exports = {
     require("tailwind-scrollbar-hide"),
     require("tailwindcss-animate"),
     require("tailwindcss-border-gradient-radius"),
+    addVariablesForColors,
   ],
 };
