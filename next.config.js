c2ac10975a623adfdf16ed095f8816542ba730d7
tomimarkus991 <tomimarkusalber@gmail.com>
2023-04-29 19:37:05 +0300

    Dependency updates

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@ module.exports = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-    // typedRoutes: true,
-    // fontLoaders: [
-    //   {
-    //     loader: "@next/font/google",
-    //     options: { subsets: ["latin"] },
-    //   },
-    // ],
+    typedRoutes: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 };
